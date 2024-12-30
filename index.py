d2273681cbe7296335e17a06abcaed909898d67c
ygzkl <emirkula41@gmail.com>
2024-12-31 00:24:37 +0300

    Delete unnecessary comments

--- index.py
+++ index.py
@@ -14,25 +14,20 @@ def check_availability(url):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Tüm script taglarını kontrol et
         script_tags = soup.find_all("script")
         for script in script_tags:
             if "Magento_Catalog/js/product/view/provider" in script.text:
-                json_start = script.text.find('{')  # JSON başlangıç
-                json_end = script.text.rfind('}') + 1  # JSON bitiş
+                json_start = script.text.find('{') 
+                json_end = script.text.rfind('}') + 1  
                 json_data = script.text[json_start:json_end]
                 
                 try:
-                    # JSON'u ayrıştır
                     data = json.loads(json_data)
 
-                    # Tek ürünün ID'sini al (keys() ile)
                     product_id = next(iter(data["*"]["Magento_Catalog/js/product/view/provider"]["data"]["items"].keys()))
 
-                    # is_available kontrolü yap
                     is_available = data["*"]["Magento_Catalog/js/product/view/provider"]["data"]["items"][product_id]["is_available"]
 
-                    # Sonucu yazdır
                     print(f"Product ID: {product_id}")
                     print(f"Product is available: {is_available}")
                     return is_available
@@ -42,7 +37,5 @@ def check_availability(url):
     else:
         print(f"Failed to fetch the page. Status code: {response.status_code}")
         return None
-
-# Örnek kullanım
 
 check_availability(url)
